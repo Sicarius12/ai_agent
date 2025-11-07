5706ca0ca0b742cf6fd1b2f9156d4c918bf41de4
Enea Didi <enedidi@gmail.com>
2025-11-07 03:17:21 -0500

    Adding a file reading function that will only read up to 10000 characters. If file is more than 10000 characters it will truncate the contents and append a notice at the end

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -21,8 +21,30 @@ def get_files_info(working_directory, directory="."):
                     f"- {entry.name}: file_size={stat_result.st_size} bytes, is_dir={entry.is_dir()}"
                 )
         return "\n".join(entries_info)
-    except Exception as exc:
-        return f"Error listing files: {exc}"
+    except Exception as e:
+        return f"Error listing files: {e}"
+    
+def get_file_content(working_directory, filepath):
+
+    abs_working_dir = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, filepath))
+
+    if os.path.commonpath([abs_working_dir, full_path]) != abs_working_dir:
+        return f'Error: Cannot list "{filepath}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(full_path):
+        return f'Error: "{filepath}" is not a file'
+    
+    MAX_CHARS = 10000
+    try:
+        contents = ""
+        with open(full_path, "r") as f:
+            contents = f.read(MAX_CHARS + 1)
+            if len(contents) > MAX_CHARS:
+                contents = contents[:MAX_CHARS] + f'\n[...File "{filepath}" truncated at 10000 characters]'
+            return contents
+    except Exception as e:
+        return f"Error reading file: {e}"
 
 def get_files_info_alt(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
